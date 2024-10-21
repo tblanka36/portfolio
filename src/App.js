d589f8a9814bafe9f8afea30ecf38e1e339e7569
tblanka36 <tblanka36@gmail.com>
2024-10-21 14:21:16 +0200

    Clean up boilerplate and prepare for portfolio setup.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <header>
+          <h1>Welcome to My Portfolio</h1>
+          <p>Explore my work in Web Design, Logo Design, and Graphic Design.</p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
